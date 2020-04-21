aada1b55b61d4376b68a897d4749a6b19d8843dd
Ora Martindale <ora@martindale.org>
2020-04-21 16:59:23 -0500

    Correct model name from Todo to Todos

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/TodoApp");
 
-const Todo = mongoose.model("Todo", {
+const Todo = mongoose.model("Todos", {
   text: { type: String, required: true, minlength: 1, trim: true },
   completed: { type: Boolean, default: false },
   completedAt: { type: Number, default: null }
