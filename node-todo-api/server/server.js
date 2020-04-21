b75f140ddd59855ec874e5e536005802f3ef3964
Ora Martindale <ora@martindale.org>
2020-04-21 16:56:56 -0500

    Added some additional schema (required, defaults, etc) to the Todos model

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -4,21 +4,11 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/TodoApp");
 
 const Todo = mongoose.model("Todo", {
-  text: { type: String },
-  completed: { type: Boolean },
-  completedAt: { type: Number }
+  text: { type: String, required: true, minlength: 1, trim: true },
+  completed: { type: Boolean, default: false },
+  completedAt: { type: Number, default: null }
 });
 
-var newTodo = new Todo({
-  text: "Cook dinner"
-});
+var otherTodo = new Todo({ text: "Edit this video" });
 
-newTodo.save().then(
-  doc => {
-    console.log("Saved Todo", doc);
-  },
-  err => {
-    console.log("Unable to save Todo");
-    console.error(err);
-  }
-);
+otherTodo.save().then(console.log, console.error);
