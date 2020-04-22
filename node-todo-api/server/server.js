1023876fdaeb1992852a6fc527c9776c0e4e92c1
Ora Martindale <ora@martindale.org>
2020-04-21 22:29:00 -0500

    Refactor

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,18 +1,7 @@
-const mongoose = require("mongoose");
+const { mongoose } = require("./db/mongoose");
+const { Todo } = require("./model/todo");
+const { User } = require("./model/user");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+// var newUser = new User({ email: "ora@martindale.org" });
 
-const Todo = mongoose.model("Todos", {
-  text: { type: String, required: true, minlength: 1, trim: true },
-  completed: { type: Boolean, default: false },
-  completedAt: { type: Number, default: null }
-});
-
-const User = mongoose.model("Users", {
-  email: { type: String, required: true, minlength: 1, trim: true }
-});
-
-var newUser = new User({ email: "ora@martindale.org" });
-
-newUser.save().then(console.log, console.error);
+// newUser.save().then(console.log, console.error);
