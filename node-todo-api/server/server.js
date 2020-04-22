d2fe7fb1a0dd3d18035d8ee664e773a2e34e61f1
Ora Martindale <ora@martindale.org>
2020-04-22 16:44:56 -0500

    Add POST /todos route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,7 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./model/todo");
 const { User } = require("./model/user");
 
-// var newUser = new User({ email: "ora@martindale.org" });
+const app = express();
 
-// newUser.save().then(console.log, console.error);
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(
+    doc => {
+      res.send(doc);
+    },
+    err => {
+      res.status(400).send(err);
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Express server listening on port 3000");
+});
